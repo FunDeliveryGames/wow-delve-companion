1d8320644e2acae3a0e141bb9ac2f4a885921eae
Fun Delivery <fun.delivery.info@gmail.com>
2025-04-19 22:36:53 +0300

    Update localization

--- locale/enUS.lua
+++ locale/enUS.lua
@@ -11,23 +11,25 @@ Lockit rules:
 --- There is another "word" widely used in a community to name something. E.g. `iLvl` is a common substitution for `Item Level` regardless of the language.
 - Key names should have prefixes to indicate a kind of a key:
 --- ui
---- debug
 --- data
 --- config
 - Keys can have a description of the context if it matters.
 - "Do not translate" tag is used to indicate only-English locales.
 ]]
 --====================== NO ADDON DATA BELOW, ONLY LOCKIT ===================================================
+
 -- Common
-lockit["ui-addon-name"] = "Delve Companion" -- Do not translate
+
 lockit["ui-common-bountiful-delve"] = "Bountiful Delve"
 
 -- Delves List
+
 lockit["ui-delve-instance-button-tooltip-click-instruction"] = "<Shift + Left Click to set waypoint to the delve>"
 lockit["ui-delve-instance-button-tooltip-current-text"] = "Waypoint set."
-lockit["ui-delve-instance-button-tooltip-current-instruction"] = "<Shift + Left Click to clear waypoint>"
+lockit["ui-delve-instance-button-tooltip-current-instruction"] = "<Shift + Left Click to clear the waypoint>"
 
 -- Delves UI
+
 lockit["ui-gilded-stash-cannot-retrieve-data"] = "Visit Khaz Algar zones to see the progress"
 lockit["ui-gilded-stash-bountiful-note"] =
 "Appears only in |cnNORMAL_FONT_COLOR:Tier 11|r Bountiful Delves|A:delves-bountiful:16:16|a."
@@ -35,13 +37,17 @@ lockit["ui-no-active-bountiful"] = "No active delves"
 lockit["ui-loot-info-button-tooltip-instruction"] = "<Click to display Delves' Loot info>"
 
 -- Loot Info
+
 lockit["ui-loot-info-description"] = "Complete a delve to get:"
 
 -- Keys Info
+
 lockit["ui-bountiful-keys-count-caches-prefix"] = "Keys from caches"
 
 -- Settings
-lockit["ui-settings-missing-addon-title"] = "Required AddOn is missing: %s" -- %s: AddonName, e.g. DelveCompanion
+
+lockit["ui-settings-missing-addon-title"] =
+"Required AddOn is missing: %s" -- %s: name of the missing AddOn, e.g. DelveCompanion
 
 lockit["ui-settings-section-title-account"] = "Account-Wide"
 lockit["ui-settings-ach-widgets"] = "Delves list: Display progress of achievements (Stories and Chests)."
@@ -52,6 +58,3 @@ lockit["ui-settings-gv-details"] = "Delves UI: Display detailed Great Vault sect
 lockit["ui-settings-dashboard-overview"] = "Delves UI: Display Overview section (Gilded Stash, Bountiful Delves)."
 lockit["ui-settings-keys-cap"] = "Display weekly limits info in tooltips."
 lockit["ui-settings-translation-title"] = "Special thanks for the translation contribution:"
-
--- Debug
-lockit["debug-unexpected-enum-element"] = "Enum `%s` doesn't contain element: %s."
