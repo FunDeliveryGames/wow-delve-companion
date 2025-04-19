db9323411f01fad38da71a09a3d2ee5d2ac4b5d4
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-04-19 23:37:24 +0500

    Update RU locale

--- locale/ruRU.lua
+++ locale/ruRU.lua
@@ -31,7 +31,8 @@ lockit["ui-settings-missing-addon-title"] = "Отсутствует необхо
 
 lockit["ui-settings-section-title-account"] = "Параметры аккаунта:"
 lockit["ui-settings-ach-widgets"] = "Список вылазок: Отображать прогресс достижений (истории и сундуки)."
-lockit["ui-settings-tomtom-description"] = "Использовать путевые точки TomTom вместо значка карты Blizzard."
+lockit["ui-settings-tomtom-description"] = "Использовать путевые точки TomTom вместо маркеров карты Blizzard."
+lockit["ui-loot-info-button-tooltip-instruction"] = "<Нажмите, чтобы отобразить информацию о добыче Вылазок>"
 
 lockit["ui-settings-section-title-character"] = "Параметры персонажа:"
 lockit["ui-settings-gv-details"] = "Интерфейс вылазок: Отображать подробные детали Великого Хранилища."
