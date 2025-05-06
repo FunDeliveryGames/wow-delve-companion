8a190b7b8bcaca7eaf0136811eec00943a9a63d2
Dionys <72759855+diomsg-code@users.noreply.github.com>
2025-05-06 22:57:13 +0200

    Update deDE.lua

--- Locale/deDE.lua
+++ Locale/deDE.lua
@@ -1,1 +1,12 @@
+if GetLocale() ~= "ruRU" then return end
 
+local _, AddonTbl = ...
+
+---@type DelveCompanion
+local DelveCompanion = AddonTbl.DelveCompanion
+
+---@type Logger
+local Logger = DelveCompanion.Logger
+---@type Lockit
+local Lockit = DelveCompanion.Lockit
+-- ====================== NO ADDON DATA BELOW, ONLY LOCKIT ===================================================
