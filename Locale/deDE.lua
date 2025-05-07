b69b2965c74cdf0519a220d94f7fabe8f62b665c
diomsg <social.network@s-email.de>
2025-05-07 23:15:51 +0200

    Update deDE.lua

--- Locale/deDE.lua
+++ Locale/deDE.lua
@@ -27,7 +27,7 @@ Lockit.UI_DELVE_INSTANCE_BUTTON_TOOLTIP_CURRENT_INSTRUCTION = "<Shift + Linkskli
 Lockit.UI_GILDED_STASH_CANNOT_RETRIEVE_DATA = "Besuche die Gebiete von Khaz Algar, um den Fortschritt zu sehen."
 Lockit.UI_GILDED_STASH_BOUNTIFUL_NOTE = "Erscheint nur in großzügigen Tiefen der |cnNORM...NT_COLOR:Stufe 11|r |A:delves-bountiful:16:16|a."
 Lockit.UI_NO_ACTIVE_BOUNTIFUL = "Keine aktiven Tiefen."
-Lockit.UI_LOOT_INFO_BUTTON_TOOLTIP_INSTRUCTION = "<Klicken, um Beute Informationen der Tiefen anzuzeigen>"
+Lockit.UI_LOOT_INFO_BUTTON_TOOLTIP_INSTRUCTION = "<Klicken, um Beuteinformationen der Tiefen anzuzeigen>"
 
 -- Loot Info
 
@@ -45,8 +45,8 @@ Lockit.UI_COMPARTMENT_DESCRIPTION_RIGHT_CLICK = "|cnGREEN_FONT_COLOR:Rechtsklick
 -- Settings
 
 Lockit.UI_SETTINGS_SECTION_TITLE_ACCOUNT = "Account-weite Einstellungen"
-Lockit.UI_SETTING_DELVE_PROGRESS_WIDGETS_NAME = "Tiefen Fortschrittsinformation"
-Lockit.UI_SETTING_DELVE_PROGRESS_WIDGETS_TOOLTIP = "Tiefen-Liste: Zeige den Fortschritt von Erfolgen für jede Tiefe (Geschichten und Truhen) an."
+Lockit.UI_SETTING_DELVE_PROGRESS_WIDGETS_NAME = "Tiefen-Fortschrittsinfo"
+Lockit.UI_SETTING_DELVE_PROGRESS_WIDGETS_TOOLTIP = "Tiefen-Liste: Zeige den Erfolgsfortschritt für jede Tiefe an (Geschichten und Truhen)."
 Lockit.UI_SETTING_WAYPOINT_TRACKING_TYPE_NAME = "Art der Wegpunktverfolgung"
 Lockit.UI_SETTING_WAYPOINT_TRACKING_TYPE_TOOLTIP_START = "Wähle aus, welche Art von Wegpunkten für die Navigation verwendet werden soll.\n\nMögliche Optionen:"
 Lockit.UI_SETTING_WAYPOINT_TRACKING_TYPE_TOOLTIP_BLIZZARD = "- Blizzards-Kartenpin (die Standard-Navigation im Spiel)"
@@ -59,17 +59,17 @@ Lockit.UI_SETTING_WAYPOINT_TRACKING_OPTION_TOMTOM_NAME = "TomTom"
 Lockit.UI_SETTING_WAYPOINT_TRACKING_OPTION_TOMTOM_DESCRIPTION = "verwende TomTom-Wegpunkte"
 
 Lockit.UI_SETTINGS_SECTION_TITLE_CHARACTER = "Charakter-spezifische Einstellungen"
-Lockit.UI_SETTING_TOOLTIP_EXTENSION_NAME = "zusätzliche Informationen in Tooltips"
-Lockit.UI_SETTING_TOOLTIP_EXTENSION_TOOLTIP = "Zeigt zusätzliche Informationen in den Tooltips an (z.B. die Anzahl der |cnITEM_EPIC_COLOR:restaurierten Kastenschlüssel|r, die diese Woche erhalten wurden).“
+Lockit.UI_SETTING_TOOLTIP_EXTENSION_NAME = "Tooltip-Zusatzinfo"
+Lockit.UI_SETTING_TOOLTIP_EXTENSION_TOOLTIP = "Zeigt zusätzliche Informationen in den Tooltips an (z.B. die Anzahl der |cnITEM_EPIC_COLOR:restaurierten Kastenschlüssel|r, die diese Woche erhalten wurden)."
 Lockit.UI_SETTING_COMPANION_CONFIG_NAME = "Begleiter-Konfigurations-Widget"
-Lockit.UI_SETTING_COMPANION_CONFIG_TOOLTIP = "Tiefen UI: Aktiviert ein Widget, das die aktuelle Spezialisierung und die Fähigkeiten des Begleiters anzeigt. Es kann verwendet werden, um die Konfiguration direkt von der Tiefen UI aus zu ändern.\nHinweis: Der Layout-Typ hat keinen Einfluss auf die Funktionalität, nur auf die visuelle Anordnung."
+Lockit.UI_SETTING_COMPANION_CONFIG_TOOLTIP = "Tiefen-UI: Aktiviert ein Widget, das die aktuelle Spezialisierung und die Fähigkeiten des Begleiters anzeigt. Es kann verwendet werden, um die Konfiguration direkt von der Tiefen-UI aus zu ändern.\nHinweis: Der Layout-Typ hat keinen Einfluss auf die Funktionalität, nur auf die visuelle Anordnung."
 Lockit.UI_SETTING_COMPANION_CONFIG_OPTION_HORIZONTAL_NAME = "Horizontales Layout"
 Lockit.UI_SETTING_COMPANION_CONFIG_OPTION_HORIZONTAL_DESCRIPTION = "Die Schaltflächen werden horizontal angeordnet. Das Begleiter-Modell wird angezeigt."
 Lockit.UI_SETTING_COMPANION_CONFIG_OPTION_VERTICAL_NAME = "Vertikales Layout"
 Lockit.UI_SETTING_COMPANION_CONFIG_OPTION_VERTICAL_DESCRIPTION = "Die Schaltflächen werden vertikal angeordnet. Das Begleiter-Modell ist verborgen."
-Lockit.UI_SETTING_GV_DETAILS_NAME = "angepasste große Schatzkammer"
-Lockit.UI_SETTING_GV_DETAILS_TOOLTIP = "Tiefen UI: Zeigt große Schatzkammer Belohnungen und den dazugehörigen Fortschritt für die Tiefen direkt in der Tiefen UI an."
-Lockit.UI_SETTING_DASHBOARD_OVERVIEW_NAME = "Übersichtsbereich der Tiefen"
-Lockit.UI_SETTING_DASHBOARD_OVERVIEW_TOOLTIP = "Tiefen UI: Zeigt einen zusätzlichen Übersichtsbereich an. Er enthält Informationen über vergoldete Schätze, verfügbare großzügige Tiefen und Tiefen-bezogene Verbrauchsgüter."
+Lockit.UI_SETTING_GV_DETAILS_NAME = "Große Schatzkammer"
+Lockit.UI_SETTING_GV_DETAILS_TOOLTIP = "Tiefen-UI: Zeigt große Schatzkammer Belohnungen und den dazugehörigen Fortschritt für die Tiefen direkt in der Tiefen-UI an."
+Lockit.UI_SETTING_DASHBOARD_OVERVIEW_NAME = "Tiefen-Übersichtsbereich"
+Lockit.UI_SETTING_DASHBOARD_OVERVIEW_TOOLTIP = "Tiefen-UI: Zeigt einen zusätzlichen Übersichtsbereich an. Er enthält Informationen über vergoldete Schätze, verfügbare großzügige Tiefen und Tiefen-bezogene Verbrauchsgüter."
 
 Lockit.UI_SETTINGS_TRANSLATION_TITLE = "Besonderen Dank für den Beitrag zur Übersetzung:"
