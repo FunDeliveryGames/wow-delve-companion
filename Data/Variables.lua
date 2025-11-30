7ea0d330646bbc81c28e0bda407fac5dc8d629d8
Fun Delivery <fun.delivery.info@gmail.com>
2025-11-30 05:17:08 +0200

    Fix tabs logic for the retail

--- Data/Variables.lua
+++ Data/Variables.lua
@@ -19,6 +19,6 @@ local Variables = {
     shardsCollected = 0,
     tomTomAvailable = false,
     mpeAvailable = false,
-    isPTR = (select(4, GetBuildInfo())) >= 110205
+    isPTR = (select(4, GetBuildInfo())) >= 110207
 }
 DelveCompanion.Variables = Variables
